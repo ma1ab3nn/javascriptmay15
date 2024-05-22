c9e25c1c70de6f54a395c5c78a62907ac1e18578
Mala Benn <malabenn@hotmail.co.uk>
2024-05-22 21:10:28 +0100

    Completed the tasks

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/*Task 1
+Add comments to one of your past JS tasks from a previous lesson*/
+
 //Psuedocode:
 //LOOP:
 //Set number to 1
@@ -33,9 +36,85 @@ for (i=1; i<= 20; i++)
 
 }
 
+/* Task 2
+Produce a pseudocode response to one or more of the following challenges. As comments.
 
 
-//function which accepts a word
+1. Reading List
+
+Keep track of which books you read and which books you want to read!
+
+Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and already read (a boolean indicating if you read it yet).
+
+Iterate through the array of books. For each book, log the book title and book author like so: “The Hobbit by J.R.R. Tolkien”.
+
+Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like ‘You already read “The Hobbit” by J.R.R. Tolkien’, and if not, log a string like ‘You still need to read “The Lord of the Rings” by J.R.R. Tolkien.’
+
+ */
+
+//function to log the book title and book author
+//List of books
+    //Title
+    //Author
+    //Read/Yet to read (T/F)
+
+//LOOP through the list of books
+//Set position to 0
+//Break until last book
+//increment by 1
+
+    //Log the Title
+    //Log the Author
+    // Check whether read or not
+        // IF you've read the book
+            //print "You've already read "Title" by "Author"
+        // ELSE 
+            //print "You still need to read "Title" by "Author"
+
+
+/*2.Recipe
+
+
+Create an object to hold information on your favourite recipes. It should have properties for:
+recipeTitle (a string)
+servings (a number)
+ingredients (an array of strings)
+directions (a string)
+List all recipes
+Create a loop to list all the ingredients. */
+
+//function to list all the ingredients of a recipe
+//Recipe
+    //Title
+    //Servings
+    //Ingredients
+    //Directions
+
+//LOOP through the list of recipes
+//Set position to 0
+//Break until last recipe
+//increment by 1
+//print the recipe title
+
+    //LOOP through the list of ingredients
+    //Set position to 0
+    //Break until last ingredient
+    //increment by 1
+    //print the ingredients
+
+/*
+3. Fix start
+
+Create a function called fixStart. It should take a single argument, a string, and return a version where all occurrences of its first character have been replaced with ‘*****’, except for the first character itself. You can assume that the string is at least one character long. For example:
+
+
+fixStart('babble'): 'ba**le'
+
+fixStart('turtle'): 'tur*le'
+
+*/
+
+//function called fixStart which accepts a word
 
 // store the first charachter in variable firstCharachter
 
@@ -50,3 +129,7 @@ for (i=1; i<= 20; i++)
         //replace charachter with *
     // ELSE 
         //print the word
+
+
+
+
